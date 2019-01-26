f7d7d435002a98ae80622eea15af090617563d9b
itijiva <itijiva.tothyo@gmail.com>
2019-01-26 14:01:43 +0530

    add show html file, httprouter, GetPost method

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"html/template"
 	"database/sql"
 	_ "github.com/lib/pq"
+	"github.com/julienschmidt/httprouter"
 	"fmt"
+	"log"
+	"strconv"
 )
 
 type Post struct {
@@ -76,12 +79,24 @@ func PostIndex(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "posts/index", posts) 
 }
 
+func GetPost(id int) (post Post, err error) {
+	post = Post{}
+	err = Db.QueryRow("select id, title, body from posts where id = $1", id).Scan(&post.Id, &post.Title, &post.Body)
+	return
+}
+
+func ShowPost(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	id, _ := strconv.Atoi(ps.ByName("id"))
+	post, _ := GetPost(id)
+	templates.ExecuteTemplate(w, "posts/show", post) 
+}
+
 func main() {
-	server := http.Server{
-		Addr: "127.0.0.1:3000",
-	}
+	router := httprouter.New()
+
 	http.HandleFunc("/posts", PostIndex)
 	http.HandleFunc("/posts/new", NewPost)
 	http.HandleFunc("/posts/create", CreatePost)
-	server.ListenAndServe()
+	router.GET("/posts/:id", ShowPost)
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
